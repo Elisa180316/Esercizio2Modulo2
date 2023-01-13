ae0cd7af3241f21334d2a9b59b8340fd5d572090
Elisa180316 <114396274+Elisa180316@users.noreply.github.com>
2023-01-13 17:57:51 +0100

    Add files via upload

---
+++ Esercizio2.js
@@ -0,0 +1,94 @@
+/*
+REGOLE
+- Tutte le risposte devono essere scritte in JavaScript
+- Se sei in difficoltà puoi chiedere aiuto a un Teaching Assistant
+- Puoi usare Google / StackOverflow ma solo quanto ritieni di aver bisogno di qualcosa che non è stato spiegato a lezione
+- Puoi testare il tuo codice in un file separato, o de-commentando un esercizio alla volta
+- Per farlo puoi utilizzare il terminale Bash, quello di VSCode o quello del tuo sistema operativo (se utilizzi macOS o Linux)
+*/
+
+/* ESERCIZIO 1
+ Elenca e descrivi i principali datatype in JavaScript. Prova a spiegarli come se volessi farli comprendere a un bambino.
+*/
+
+/* - Number: Qualsiasi numero senza differenza tra intero e con la virgola.
+let num1 = 16;
+let num2 = 2.20;
+
+- Stringa: Sequenza di caratteri racchiusi tra apici singoli o doppi.
+let nome1 = "Elisa";
+let nome2 = 'Lily';
+
+- Boolean: Si basa sull'utilizzo di due valori: true oppure false.
+let età = 18;
+if (età >=18);
+console.log("Benvenuto nel sito");
+
+
+- Undefined: Rappresenta un valore che non esiste, nonostante sia stata dichiarata la variabile.
+let = sugar;
+alert('Il valore di sugar: '+sugar)
+
+- Null: E' un valore vuoto, la variabile non contiene niente.
+
+- */
+
+/* ESERCIZIO 2
+ Descrivi cos'è un oggetto in JavaScript, con parole tue.
+*/
+
+/* Un oggetto è una struttura più complessa, rispetto alle strutture primitive. Sono raccolte di coppie formate da chiave-valore.
+let persona = {
+            nome: "Elisa",
+            eta: "40",
+            sesso: "Femmina",
+        }
+        console.log(persona);
+}*/
+
+/* ESERCIZIO 3
+ Scrivi il codice necessario ad effettuare un addizione (una somma) dei numeri 12 e 20.
+*/
+
+/* let num1 = 12;
+let num2 = 20;
+let somma = num1 + num2;
+console.log(somma); */
+
+/* ESERCIZIO 4
+ Crea una variable di nome "x" e assegna ad essa il numero 12.
+*/
+
+/* Per creare una variabile in Javascript ed assegnare un valore si può utilizzare la parola chiave "let" seguita dal nome della variabile e de simbolo di assegnazione(=) 
+let x = 12;
+console.log(x); */
+
+/* ESERCIZIO 5
+ Crea una variable chiamata "name" e assegna ad essa il tuo nome, sotto forma di stringa.
+*/
+
+/* Per creare una variabile in Javascript ed assegnare un valore si può utilizzare la parola chiave "let" seguita dal nome della variabile e de simbolo di assegnazione(=) Es:
+
+let name = "Elisa";
+console.log(name); */
+
+/* ESERCIZIO 6
+ Esegui una sottrazione tra i numeri 4 e la variable "x" appena dichiarata (che contiene il numero 12).
+*/
+
+/* let num1 = 4;
+let x = 12;
+let differenza = num1 - x;
+console.log(differenza);
+ */
+
+/* ESERCIZIO 7
+ Crea due variabili: "name1" e "name2". Assegna a name1 la stringa "john", e assegna a name2 la stringa "John" (con la J maiuscola!).
+ Verifica che name1 sia diversa da name2 (suggerimento: è la stessa cosa di verificare che la loro uguaglianza sia falsa).
+ Infine, verifica che la loro uguaglianza diventi true se entrambe vengono trasformate in lowercase (senza cambiare il valore di name2!).
+ NON HAI BISOGNO DI UN BLOCCO IF/ELSE. E' sufficiente utilizzare console.log().
+*/
+
+/* let name1 = "john";
+let name2 = "John";
+console.log(name1 != name2); */
